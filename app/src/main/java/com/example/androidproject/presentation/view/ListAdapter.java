7a1680e9f0edbb72b0ceb4be817da9d330c685ec
ofenzar <fenzar@et.esiea.fr>
2020-05-21 09:23:47 +0200

    Suppression Title Project => ecran detail

--- app/src/main/java/com/example/androidproject/presentation/view/ListAdapter.java
+++ app/src/main/java/com/example/androidproject/presentation/view/ListAdapter.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.androidproject.R;
 import com.example.androidproject.presentation.model.Player;
 import com.example.androidproject.presentation.model.Team;
